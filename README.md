c5d874ab540a289f4f9a00e622bae65b7106fe93
Caleb Knight <knightca@oregonstate.edu>
2022-01-13 19:19:42 -0800

    Added hello.c and did part 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CS344_Project2
+Caleb Knight
+
+This is a simple project to practice what I have learned in week 2 of CS 344.
---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() 
+{
+	int x = 12;
+	float y = 3.14;
+	char* s = "Hello, World!";
+
+	printf("x is %d, y is %f\n", x, y);
+	printf("%s\n", s);
+
+	for(int i = 0; i < 5; i++) {
+		printf("%d x %d = %d\n", i, 5, i*5);
+	}
+}
